85c7cae6dfd0e0f01cd8897a4de608203a75d713
Andrew Jocson <drew.jocson@gmail.com>
2017-08-31 23:33:13 -0700

    Adding autohide_dock.sh

---
+++ autohide_dock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will auto hide the dock and set it to delay to 0 ms
+# Created by: 	Andrew Jocson
+# Date:		5/05/2017
+
+# Grab current logged in user
+USER=`/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4`
+
+# Autohide dock
+sudo -u $USER defaults write com.apple.dock autohide -bool true
+
+# Set delay to 0 ms
+sudo -u $USER defaults write com.apple.Dock autohide-delay -float 0
+
+# Kill Dock and reopen
+killall Dock
